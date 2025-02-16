efe376d18a1edab1a9b177350f698d2c74515e60
EdwardShanahan9 <193254185+EdwardShanahan9@users.noreply.github.com>
2025-02-16 13:44:50 +0000

    Add MobileMenuItem component

---
+++ src/components/MobileMenuItem/MobileMenuItem.jsx
@@ -0,0 +1,9 @@
+const MobileMenuItem = ({ number, text }) => {
+  return (
+    <li className="Mobile-item__item">
+      0{number} {text}
+    </li>
+  );
+};
+
+export default MobileMenuItem;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,21 @@
 import LogoIcon from "../../assets/Icons/logo.svg";
 import MenuIcon from "../../assets/Icons/icon-hamburger.svg";
+import MobileMenuItem from "../MobileMenuItem/MobileMenuItem";
 
 const Navbar = () => {
+  const navItem = ["HOME", "DESTINATION", "CREW", "TECHNOLOGY"];
+
   return (
     <nav className="Navbar">
       <img className="Navbar__logo" src={LogoIcon} alt="Logo" />
 
       <img className="Navbar__menu" src={MenuIcon} alt="Menu Icon" />
+
+      <ul className="Navbar__mobile-list">
+        {navItem.map((item, index) => (
+          <MobileMenuItem key={index} text={item} number={index} />
+        ))}
+      </ul>
     </nav>
   );
 };
